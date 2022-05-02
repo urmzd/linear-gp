7fcd2794dc990e4ce9d63b39dda3365d401f156e
Urmzd <urmzds@gmail.com>
2022-05-02 19:12:13 -0400

    Add structures to hold entities needed.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+trait Operation<V> {
+    fn apply_operation(source: V, target: V) -> V;
+}
+
+struct Instruction<V> {
+    source: V,
+    target: V,
+}
+
+struct Program<'a, V, O>
+where
+    O: Operation<V>,
+{
+    instructions: &'a [O],
+    registers: &'a [V],
+}
+
 fn main() {
     println!("Hello, world!");
 }
