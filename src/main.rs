4ea48c6f49d99748be6ca1386bb3156e6ab4e408
Urmzd <urmzds@gmail.com>
2022-05-08 16:22:49 -0400

    Describe data ownership.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use core::fmt;
+use std::rc::Rc;
+
 /// Lets describe the steps we're trying to execuet.
 ///
 /// First we initialize a population of programs.
@@ -24,48 +27,77 @@
 /// As demonstrated above, inputs can expand in another dimension (in the above case, its the #. of
 /// rows) as long as they share a dimension (in the above case, its the #. of columns)
 ///
+/// Smoke Test Algorithm:
+///     1. Load input data
+///     2. Generate programs (instructions, registers, etc..) -- Init Popuulation
+///     3. Eval Fitness
+///     4. Drop x%
+///     5. Clone 1 - x %
+///     6. Repeat from 3
+///
+///
+/// Notes:
+///     Inputs should be referenced. (RC?)
+///
 
 #[derive(PartialEq, Eq, Debug)]
 struct Dimensions(u32, Option<u32>);
 
+#[derive(Debug, Clone)]
 struct Collection<ItemType>(Vec<ItemType>);
-
-// Concrete types (for testing purposes).
-struct Add;
-struct Subtract;
-struct Multiply;
-struct Divide;
 
 type InternalRegisters = Collection<f32>;
 
-enum DataSet<InputType> {
-    Register(InternalRegisters),
-    Input(Collection<InputType>),
+#[derive(Debug, Clone)]
+enum Exemplars<'a, InputType> {
+    Register(&'a InternalRegisters),
+    Input(&'a Collection<InputType>),
 }
 
-trait Operation {
-    type InputType;
-
-    fn apply(&self, data_set: DataSet<Self::InputType>, source: i8, target: i8) -> ();
+trait Operation<InputType = InternalRegisters>: fmt::Debug
+where
+    InputType: Clone + fmt::Debug,
+{
+    fn apply(
+        &self,
+        data_set: Exemplars<InputType>,
+        registers: InternalRegisters,
+        source: i8,
+        target: i8,
+    ) -> ();
 }
 
-struct Instruction<InputType> {
+#[derive(Clone, Debug)]
+struct Instruction<'a, InputType> {
     source: i8,
     target: i8,
-    mode: DataSet<InputType>,
+    mode: &'a Exemplars<'a, InputType>,
+    registers: &'a InternalRegisters,
 }
 
-struct Program<InputType> {
-    instructions: Vec<Box<dyn Operation<InputType = InputType>>>,
+#[derive(Debug)]
+struct Program<InputType>
+where
+    InputType: Clone + fmt::Debug,
+{
+    instructions: Vec<Box<dyn Operation<InputType>>>,
     registers: InternalRegisters,
-    inputs: Collection<Collection<InputType>>,
+    inputs: Rc<Collection<Collection<InputType>>>,
+}
+
+impl<'a, T> Clone for Program<T>
+where
+    T: Clone + fmt::Debug,
+{
+    fn clone(&self) -> Self {
+        todo!()
+    }
 }
 
 trait GeneticProgramming {
     fn init_population(pop_size: usize) -> ();
     fn eval_fitness() -> Vec<f32>;
-    fn select_from_pop() -> ();
-    fn drop_from_pop() -> ();
+    fn select_from_pop(percentage: f32) -> ();
     fn breed() -> ();
     fn compete() -> ();
     fn run(n_generations: i8) -> ();
