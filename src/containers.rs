73aa13aad93049fe2adad0babdfe38e32efaf2cf
Urmzd <urmzds@gmail.com>
2022-05-23 12:25:30 -0400

    Stop evaluating individuals after 'best' fitness has been reached.

--- src/containers.rs
+++ src/containers.rs
@@ -5,11 +5,11 @@ pub struct CollectionIndexPair(Registers, usize);
 
 impl CollectionIndexPair {
     pub fn new(data: Registers, index: usize) -> Self {
-        Self(data, index)
+        CollectionIndexPair(data, index)
     }
 
     pub fn get_value(&self) -> RegisterValue {
-        let Self(internal_registers, index) = self;
+        let CollectionIndexPair(internal_registers, index) = self;
 
         internal_registers.get_value_at_index(*index)
     }
