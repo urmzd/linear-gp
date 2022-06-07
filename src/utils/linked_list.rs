3c236a9bd39ec9b0b28390d8a80b0a1398d03f48
Urmzd <urmzds@gmail.com>
2022-06-07 11:19:41 -0400

    Attempt creating a stack-based linked-list.

--- src/utils/linked_list.rs
+++ src/utils/linked_list.rs
@@ -1,10 +1,20 @@
-// Double LinkedList
-enum LinkedList<'a, T> {
-    Node {
-        data: T,
-        next: &'a LinkedList<'a, T>,
-    },
-    Tail,
+// Single-linked list.
+struct LinkedList<'a, T> {
+    length: usize,
+    head: Node<'a, T>,
+    tail: Node<'a, T>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Node<'a, T> {
+    Cons(T, &'a Node<'a, T>),
+    Nil,
+}
+
+impl<'a, T> Node<'a, T> {
+    pub fn new() -> Self {
+        Node::Nil
+    }
 }
 
 // Why are we using this data structure?
@@ -16,10 +26,38 @@ enum LinkedList<'a, T> {
 // make a -> z -> f
 // make e -> w
 
-impl<'a, T> LinkedList<'a, T> {
-    pub fn new() -> Self {}
+impl<'a, T> LinkedList<'a, T>
+where
+    T: PartialEq,
+{
+    pub fn new() -> Self {
+        LinkedList {
+            length: 0,
+            head: Node::Nil,
+            tail: Node::Nil,
+        }
+    }
 
-    pub fn append(&mut self, data: Self) {}
+    /// Adds a new element to the end of the linked list.
+    pub fn append(&'a mut self, data: T) {
+        // Current List: (Nil, Nil)
+        if self.head == Node::Nil {
+            let new_head = Node::Cons(data, &mut self.tail);
+            self.head = new_head
+            // After: (Cons T &Nil)
+        } else {
+            // Current List: (... Cons(... Cons(T, Nil)))
+        }
+        /*
+         *if self.head == List::Nil {
+         *    let new_head = List::Cons(data, &List::Nil);
+         *    self.head = new_head
+         *} else {
+         *    self.tail.append(data)
+         *}
+         */
+        todo!("fix")
+    }
 
     pub fn extract_between(&mut self, start_index: usize, end_index: usize) -> Self {
         todo!()
@@ -31,5 +69,9 @@ impl<'a, T> LinkedList<'a, T> {
 
     pub fn tail(&mut self) -> &mut Self {
         todo!("")
+    }
+
+    pub fn len(&self) -> usize {
+        self.length
     }
 }
