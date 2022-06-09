6f2092c309b33666c0a52bd96bb60490926c0e3b
Urmzd Mukhammadnaim <urmzd@dal.ca>
2022-06-09 13:15:36 -0400

    Clean up LinkedList tests.

--- src/utils/linked_list.rs
+++ src/utils/linked_list.rs
@@ -10,7 +10,6 @@ struct LinkedList<T> {
 struct Node<T> {
     data: T,
     next: Option<Pointer<T>>,
-    length: usize,
 }
 
 type Pointer<T> = NonNull<Node<T>>;
@@ -20,52 +19,25 @@ where
     T: PartialEq,
 {
     fn new(data: T) -> Self {
-        Node {
-            data,
-            next: None,
-            length: 1,
-        }
+        Node { data, next: None }
     }
 
     fn new_dyn(data: T) -> Box<Node<T>> {
         Box::new(Self::new(data))
     }
 
-    fn as_ptr(self: Box<Node<T>>) -> Option<Pointer<T>> {
-        unsafe {
-            let static_node = Box::leak(self).into();
-            let some_static_node = Some(static_node);
-            some_static_node
-        }
+    fn as_ptr(self: Box<Node<T>>) -> Pointer<T> {
+        unsafe { Box::leak(self).into() }
     }
 
     // What should this return?
     // The unwrapped node
-    fn point_next_to(&mut self, mut node: Option<Pointer<T>>) -> &Node<T> {
-        unsafe {
-            match node {
-                None => panic!("Ensure you're using as_ptr to construct the raw pointer."),
-                Some(ref mut inner_node) => {
-                    self.next = Some(*inner_node);
-                    self.length += mem::replace(&mut inner_node.as_mut().length, 1);
-                    inner_node.as_ref()
-                }
-            }
-        }
+    fn point_next_to(&mut self, mut node: Pointer<T>) {
+        self.next = Some(node);
     }
 
     fn next(&self) -> Option<&Node<T>> {
-        unsafe {
-            if let Some(inside) = &self.next {
-                return Some(inside.as_ref());
-            }
-        }
-
-        return None;
-    }
-
-    pub fn len(&self) -> usize {
-        self.length
+        unsafe { self.next.map(|node| node.as_ref()) }
     }
 }
 
@@ -90,34 +62,24 @@ where
         }
     }
 
-    /// Adds a new element to the end of the linked list.
-    ///
-    /// Cases:
-    ///     Case 1: Empty LinkedList
-    ///     Case 2: Head Only LinkedList
-    ///     Case 3: Full LinkedList
     pub fn append(&mut self, data: T) {
         unsafe {
             let node = Node::new_dyn(data);
             let some_leaked_node = node.as_ptr();
             match self.head {
-                // Case 1:
                 None => {
-                    self.head = some_leaked_node;
+                    self.head = Some(some_leaked_node);
                 }
                 Some(head_ptr) => {
                     match self.tail {
-                        // Case 2:
                         None => {
                             (*head_ptr.as_ptr()).point_next_to(some_leaked_node);
                         }
-
-                        // Case 3:
                         Some(tail_ptr) => {
                             (*tail_ptr.as_ptr()).point_next_to(some_leaked_node);
                         }
                     }
-                    self.tail = some_leaked_node;
+                    self.tail = Some(some_leaked_node);
                 }
             }
 
@@ -155,8 +117,6 @@ where
 
 #[cfg(test)]
 mod test {
-    use std::ptr;
-
     use super::{LinkedList, Node};
 
     #[test]
@@ -171,19 +131,6 @@ mod test {
         assert_eq!(linked_list.pop_head().map(|node| node.data), Some(1));
         assert_eq!(linked_list.pop_head().map(|node| node.data), Some(2));
         assert_eq!(linked_list.pop_head().map(|node| node.data), Some(3));
-
-        /*
-         *        let items = [1, 2, 3, 4, 5];
-         *        let linked_list = LinkedList::new();
-         *
-         *        for item in items {
-         *            linked_list.append(item)
-         *        }
-         *
-         *        assert_eq!(linked_list.len(), items.len());
-         *
-         *        let head = linked_list.head();
-         */
     }
 
     #[test]
@@ -191,9 +138,9 @@ mod test {
         let mut first_node = Node::new_dyn(1);
         let second_node = Node::new_dyn(2);
 
-        let second_node_ptr = first_node.point_next_to(second_node.as_ptr());
+        first_node.point_next_to(second_node.as_ptr());
 
-        assert!(ptr::eq(second_node_ptr, first_node.next().unwrap()));
-        assert_eq!(first_node.len(), 2);
+        assert_eq!(first_node.next().map(|node| node.data), Some(2));
+        assert_eq!(first_node.next().and_then(|node| node.next()), None)
     }
 }
