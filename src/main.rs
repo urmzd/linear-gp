61b5c8df6a18ea1be280c90cb8030d4ed40e5d0d
Urmzd Mukhammadnaim <hello@urmzd.com>
2022-06-29 14:58:12 -0400

    Remove example from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,3 @@
-use std::error;
-
-use lgp::{
-    data::iris::{
-        data::{IrisInput, IrisLgp},
-        ops::IRIS_EXECUTABLES,
-        utils::{get_iris_content, ContentFilePair},
-    },
-    genes::{
-        algorithm::{EventHooks, GeneticAlgorithm, HyperParameters, Loader},
-        program::{Program, ProgramGenerateParams},
-    },
-};
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn error::Error>> {
-    let ContentFilePair(_, file) = get_iris_content().await?;
-    let inputs = IrisLgp::load_inputs(file.path());
-
-    let hyper_params: HyperParameters<Program<IrisInput>> = HyperParameters {
-        population_size: 1000,
-        gap: 0.5,
-        n_crossovers: 0.5,
-        n_mutations: 0.5,
-        max_generations: 5,
-        program_params: ProgramGenerateParams::new(&inputs, 100, IRIS_EXECUTABLES, None),
-    };
-
-    IrisLgp::execute(&hyper_params, EventHooks::default())?;
-    Ok(())
+fn main() {
+    ()
 }
