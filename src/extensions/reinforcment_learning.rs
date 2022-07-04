43134901c0c1eaf5918f4cfadc0b495418a59d3c
Urmzd Mukhammadnaim <hello@urmzd.com>
2022-07-04 00:06:35 -0400

    Add support for Reinforcement Learning inputs.

--- src/extensions/reinforcment_learning.rs
+++ src/extensions/reinforcment_learning.rs
@@ -2,7 +2,13 @@ use std::marker::PhantomData;
 
 use derive_new::new;
 
-use crate::{core::program::ExtensionParameters, utils::common_traits::ValidInput};
+use crate::{
+    core::{
+        characteristics::Fitness,
+        program::{ExtensionParameters, Program},
+    },
+    utils::common_traits::ValidInput,
+};
 
 #[derive(Debug, Clone, new)]
 pub struct ReinforcementLearningParameters<T>
@@ -17,4 +23,27 @@ where
     T: ValidInput,
 {
     type InputType = T;
+}
+
+pub trait ReinforcmentLearningInput: ValidInput {
+    fn init_game(&self) -> ();
+    fn act(&mut self, action: Self::Actions);
+    fn finish(&mut self);
+}
+
+impl<'a, T> Fitness for Program<'a, ReinforcementLearningParameters<T>>
+where
+    T: ReinforcmentLearningInput,
+{
+    fn eval_fitness(&self) -> crate::core::characteristics::FitnessScore {
+        todo!()
+    }
+
+    fn eval_set_fitness(&mut self) -> crate::core::characteristics::FitnessScore {
+        todo!()
+    }
+
+    fn get_fitness(&self) -> Option<crate::core::characteristics::FitnessScore> {
+        todo!()
+    }
 }
