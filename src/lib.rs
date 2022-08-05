d9216d39bca146e31c9f4b39f9f8b2f64fccaeca
Urmzd Mukhammadnaim <urmzd@dal.ca>
2022-08-04 20:00:59 -0400

    Add lint warning for missing documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#![warn(rustdoc::all)]
+#![warn(missing_docs)]
+
+//! A library to solve problems using linear genetic programming!
+//! 
+//! Provides a bootstrapped implementation to help you start exploring problems immediately.
 pub mod core;
 pub mod extensions;
 pub mod utils;
