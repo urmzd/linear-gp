1a6cc3174aba2f1c37d750645caca2e507cebb5e
Urmzd Mukhammadnaim <urmzds@gmail.com>
2023-01-07 15:32:18 -0400

    refactor: rename tests

--- examples/mountain_car/comparison.rs
+++ examples/mountain_car/comparison.rs
@@ -83,7 +83,7 @@ fn main() -> VoidResultAnyError {
     const PLOT_FILE_NAME: &'static str = "plots/comparisons/mountain_car.png";
     plot_benchmarks(lgp_pops, PLOT_FILE_NAME, -200.0..0.0)?;
 
-    const Q_PLOT_FILE_NAME: &'static str = "plots/comparisons/q_mountain_car.png";
+    const Q_PLOT_FILE_NAME: &'static str = "plots/comparisons/mountain_car_q.png";
     plot_benchmarks(q_pops, Q_PLOT_FILE_NAME, -200.0..0.0)?;
     Ok(())
 }
