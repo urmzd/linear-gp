6e9d0b4802e7b1c3ed0a7d7a744bd2d767efce50
Urmzd Mukhammadnaim <hello@urmzd.com>
2023-03-07 16:23:10 -0400

    fix: default parameters

--- examples/cart_pole/search.rs
+++ examples/cart_pole/search.rs
@@ -24,7 +24,7 @@ fn main() -> VoidResultAnyError {
     let mut alpha_optim = tpe::TpeOptimizer::new(tpe::parzen_estimator(), tpe::range(0.0, 1.0)?);
     let mut gamma_optim = tpe::TpeOptimizer::new(tpe::parzen_estimator(), tpe::range(0.9, 0.99)?);
     let mut alpha_decay_optim =
-        tpe::TpeOptimizer::new(tpe::parzen_estimator(), tpe::range(1e-5, 1e-1)?);
+        tpe::TpeOptimizer::new(tpe::parzen_estimator(), tpe::range(0.00001, 0.001)?);
 
     let game = CartPoleEnv::new(RenderMode::None);
     let environment = CartPoleInput::new(game);
@@ -36,57 +36,52 @@ fn main() -> VoidResultAnyError {
     let mut best_alpha = 0.1;
     let mut best_gamma = 0.95;
     let mut best_alpha_decay = 1e-3;
-    let mut best_instruction_count = 8;
     let mut best_result = 0.;
 
-    for instruction_count in 6..16 {
-        for _ in 0..100 {
-            let alpha = alpha_optim.ask(&mut generator())?;
-            let alpha_decay = alpha_decay_optim.ask(&mut generator())?;
-            let gamma = gamma_optim.ask(&mut generator())?;
+    for _ in 0..1000 {
+        let alpha = alpha_optim.ask(&mut generator())?;
+        let alpha_decay = alpha_decay_optim.ask(&mut generator())?;
+        let gamma = gamma_optim.ask(&mut generator())?;
 
-            let parameters =
-                InteractiveLearningParameters::new(initial_states.clone(), environment.clone());
+        let parameters =
+            InteractiveLearningParameters::new(initial_states.clone(), environment.clone());
 
-            let hyper_params = HyperParameters {
-                population_size: 100,
-                gap: 0.5,
-                mutation_percent: 0.5,
-                crossover_percent: 0.5,
-                n_generations,
-                lazy_evaluate: false,
-                fitness_parameters: parameters,
-                program_parameters: QProgramGeneratorParameters::new(
-                    ProgramGeneratorParameters::new(
-                        instruction_count,
-                        InstructionGeneratorParameters::from::<CartPoleInput>(1),
-                    ),
-                    QConsts::new(alpha, gamma, 0.05, alpha_decay, 0.0),
+        let hyper_params = HyperParameters {
+            population_size: 100,
+            gap: 0.5,
+            mutation_percent: 0.5,
+            crossover_percent: 0.5,
+            n_generations,
+            lazy_evaluate: false,
+            fitness_parameters: parameters,
+            program_parameters: QProgramGeneratorParameters::new(
+                ProgramGeneratorParameters::new(
+                    6,
+                    InstructionGeneratorParameters::from::<CartPoleInput>(1),
                 ),
-            };
+                QConsts::new(alpha, gamma, 0.05, alpha_decay, 0.0),
+            ),
+        };
 
-            let population = QLgp::build(hyper_params).last().unwrap();
-            let result = population.best().unwrap().get_fitness().unwrap_or(0.);
+        let population = QLgp::build(hyper_params).last().unwrap();
+        let result = population.best().unwrap().get_fitness().unwrap_or(0.);
 
-            alpha_optim.tell(alpha, result)?;
-            gamma_optim.tell(gamma, result)?;
-            alpha_decay_optim.tell(alpha_decay, result)?;
+        alpha_optim.tell(alpha, result)?;
+        gamma_optim.tell(gamma, result)?;
+        alpha_decay_optim.tell(alpha_decay, result)?;
 
-            info!(
-                fitness = valuable(&result),
-                alpha = valuable(&alpha),
-                gamma = valuable(&gamma),
-                alpha_decay = valuable(&alpha_decay),
-                instruction_count = valuable(&instruction_count)
-            );
+        info!(
+            fitness = valuable(&result),
+            alpha = valuable(&alpha),
+            gamma = valuable(&gamma),
+            alpha_decay = valuable(&alpha_decay),
+        );
 
-            if result > best_result {
-                best_alpha = alpha;
-                best_gamma = gamma;
-                best_result = result;
-                best_alpha_decay = alpha_decay;
-                best_instruction_count = instruction_count;
-            }
+        if result > best_result {
+            best_alpha = alpha;
+            best_gamma = gamma;
+            best_result = result;
+            best_alpha_decay = alpha_decay;
         }
     }
 
@@ -95,7 +90,6 @@ fn main() -> VoidResultAnyError {
         best_alpha = valuable(&best_alpha),
         best_gamma = valuable(&best_gamma),
         best_alpha_decay = valuable(&best_alpha_decay),
-        best_instruction_count = valuable(&best_instruction_count)
     );
 
     Ok(())
