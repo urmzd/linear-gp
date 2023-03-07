79466308317512f652c3c0e8c4ad2b7f635cdc3a
Urmzd Mukhammadnaim <urmzds@gmail.com>
2023-03-07 14:58:39 -0400

    refactor: move compare into main examples

--- examples/cart_pole/main.rs
+++ examples/cart_pole/main.rs
@@ -1,47 +1,75 @@
 use gym_rs::{envs::classical_control::cartpole::CartPoleEnv, utils::renderer::RenderMode};
-
-use config::CartPoleInput;
+use itertools::Itertools;
 use lgp::{
     core::{
         algorithm::{GeneticAlgorithm, HyperParameters},
         instruction::InstructionGeneratorParameters,
-        program::ProgramGeneratorParameters,
+        program::{Program, ProgramGeneratorParameters},
     },
     extensions::{
         gym_rs::ExtendedGymRsEnvironment,
-        interactive::InteractiveLearningParameters,
-        q_learning::{QConsts, QLgp, QProgramGeneratorParameters},
+        interactive::{ILgp, InteractiveLearningInput, InteractiveLearningParameters},
+        q_learning::{QConsts, QLgp, QProgram, QProgramGeneratorParameters},
     },
-    utils::types::VoidResultAnyError,
+    utils::{plots::plot_benchmarks, types::VoidResultAnyError},
 };
-
 mod config;
+use config::CartPoleInput;
 
 fn main() -> VoidResultAnyError {
-    let environment = CartPoleEnv::new(RenderMode::Human);
+    let environment = CartPoleEnv::new(RenderMode::None);
     let input = CartPoleInput::new(environment);
-    let n_generations = 1;
+    let n_generations = 100;
     let n_trials = 5;
     let initial_states = CartPoleInput::get_initial_states(n_generations, n_trials);
+    let fitness_parameters = InteractiveLearningParameters::new(initial_states, input);
+    let program_parameters = ProgramGeneratorParameters::new(
+        8,
+        InstructionGeneratorParameters::from::<CartPoleInput>(1),
+    );
 
-    let hyper_params = HyperParameters {
-        population_size: 1,
-        gap: 0.5,
-        crossover_percent: 0.5,
-        mutation_percent: 0.5,
-        n_generations: 1,
-        lazy_evaluate: false,
-        fitness_parameters: InteractiveLearningParameters::new(initial_states, input),
+    let lgp_hp: HyperParameters<Program<InteractiveLearningParameters<CartPoleInput>>> =
+        HyperParameters {
+            population_size: 100,
+            gap: 0.5,
+            crossover_percent: 0.5,
+            mutation_percent: 0.5,
+            n_generations,
+            lazy_evaluate: false,
+            fitness_parameters,
+            program_parameters,
+        };
+
+    let lgpq_hp: HyperParameters<QProgram<CartPoleInput>> = HyperParameters {
+        population_size: lgp_hp.population_size,
+        gap: lgp_hp.gap,
+        crossover_percent: lgp_hp.crossover_percent,
+        mutation_percent: lgp_hp.mutation_percent,
+        n_generations: lgp_hp.n_generations,
+        lazy_evaluate: lgp_hp.lazy_evaluate,
+        fitness_parameters: lgp_hp.fitness_parameters.clone(),
         program_parameters: QProgramGeneratorParameters::new(
-            ProgramGeneratorParameters::new(
-                8,
-                InstructionGeneratorParameters::from::<CartPoleInput>(1),
-            ),
+            lgp_hp.program_parameters.clone(),
             QConsts::default(),
         ),
     };
 
-    QLgp::build(hyper_params).last();
+    let lgp_pops = ILgp::build(lgp_hp).collect_vec();
+    let q_pops = QLgp::build(lgpq_hp).collect_vec();
+
+    const PLOT_FILE_NAME: &'static str = "assets/plots/examples/cart_pole/default.png";
+    const Q_PLOT_FILE_NAME: &'static str = "assets/plots/examples/cart_pole/q.png";
+
+    plot_benchmarks(
+        lgp_pops,
+        PLOT_FILE_NAME,
+        0.0..(CartPoleInput::MAX_EPISODE_LENGTH as f64),
+    )?;
+    plot_benchmarks(
+        q_pops,
+        Q_PLOT_FILE_NAME,
+        0.0..(CartPoleInput::MAX_EPISODE_LENGTH as f64),
+    )?;
 
     Ok(())
 }
