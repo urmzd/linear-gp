f0f983f3a656d07c1d2e89a986cfe5436f1c64af
Urmzd Mukhammadnaim <urmzds@gmail.com>
2023-03-09 22:34:15 -0400

    feat: optimizer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Args, Parser, Subcommand, ValueEnum};
+use clap::{Args, Parser, ValueEnum};
 use lgp::{
     core::{
         algorithm::{GeneticAlgorithm, HyperParameters},
@@ -17,8 +17,6 @@ use lgp::{
 #[command(author, version, about, long_about=None)]
 #[command(propagate_version = true)]
 struct Cli {
-    #[command(subcommand)]
-    commands: Commands,
     #[arg(value_enum)]
     problem_type: ProblemType,
     #[command(flatten)]
@@ -39,6 +37,10 @@ struct BasicArgs {
     crossover_percent: f64,
     #[arg(long, default_value = "100")]
     n_generations: usize,
+    #[command(flatten)]
+    consts: QConsts,
+    #[arg(long, default_value = "5")]
+    n_trials: usize,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -47,94 +49,82 @@ pub enum ProblemType {
     CartPole,
 }
 
-#[derive(Subcommand)]
-enum Commands {
-    QLearning {
-        #[command(flatten)]
-        consts: QConsts,
-        #[arg(long, default_value = "5")]
-        n_trials: usize,
-    },
-}
-
 fn main() {
     let cli = Cli::parse();
 
-    match cli.commands {
-        Commands::QLearning { consts, n_trials } => {
-            if cli.problem_type == ProblemType::MountainCar {
-                let input = MountainCarInput::new();
-                let n_generations = cli.basic_args.n_generations;
-                let initial_states = MountainCarInput::get_initial_states(n_generations, n_trials);
+    if cli.problem_type == ProblemType::MountainCar {
+        let input = MountainCarInput::new();
+        let n_generations = cli.basic_args.n_generations;
+        let initial_states =
+            MountainCarInput::get_initial_states(n_generations, cli.basic_args.n_trials);
 
-                let hyper_params = HyperParameters {
-                    population_size: cli.basic_args.population_size,
-                    gap: cli.basic_args.gap,
-                    crossover_percent: cli.basic_args.crossover_percent,
-                    mutation_percent: cli.basic_args.mutation_percent,
-                    n_generations,
-                    fitness_parameters: InteractiveLearningParameters::new(initial_states, input),
-                    program_parameters: QProgramGeneratorParameters::new(
-                        ProgramGeneratorParameters::new(
-                            cli.program_parameter.max_instructions,
-                            InstructionGeneratorParameters::from::<MountainCarInput>(
-                                cli.program_parameter
-                                    .instruction_generator_parameters
-                                    .n_extras,
-                                cli.program_parameter
-                                    .instruction_generator_parameters
-                                    .external_factor,
-                            ),
-                        ),
-                        consts,
+        let hyper_params = HyperParameters {
+            population_size: cli.basic_args.population_size,
+            gap: cli.basic_args.gap,
+            crossover_percent: cli.basic_args.crossover_percent,
+            mutation_percent: cli.basic_args.mutation_percent,
+            n_generations,
+            fitness_parameters: InteractiveLearningParameters::new(initial_states, input),
+            program_parameters: QProgramGeneratorParameters::new(
+                ProgramGeneratorParameters::new(
+                    cli.program_parameter.max_instructions,
+                    InstructionGeneratorParameters::from::<MountainCarInput>(
+                        cli.program_parameter
+                            .instruction_generator_parameters
+                            .n_extras,
+                        cli.program_parameter
+                            .instruction_generator_parameters
+                            .external_factor,
                     ),
-                };
+                ),
+                cli.basic_args.consts,
+            ),
+        };
 
-                let best_score = QLgp::build(hyper_params)
-                    .last()
-                    .as_ref()
-                    .and_then(|p| p.best())
-                    .map(|p| p.get_fitness().unwrap_or(f64::NAN))
-                    .unwrap_or(f64::NAN);
+        let best_score = QLgp::build(hyper_params)
+            .last()
+            .as_ref()
+            .and_then(|p| p.best())
+            .map(|p| p.get_fitness().unwrap_or(f64::NAN))
+            .unwrap_or(f64::NAN);
 
-                println!("{}", best_score)
-            } else {
-                let input = CartPoleInput::new();
-                let n_generations = cli.basic_args.n_generations;
-                let initial_states = CartPoleInput::get_initial_states(n_generations, n_trials);
+        println!("{}", best_score)
+    } else {
+        let input = CartPoleInput::new();
+        let n_generations = cli.basic_args.n_generations;
+        let initial_states =
+            CartPoleInput::get_initial_states(n_generations, cli.basic_args.n_trials);
 
-                let hyper_params = HyperParameters {
-                    population_size: cli.basic_args.population_size,
-                    gap: cli.basic_args.gap,
-                    crossover_percent: cli.basic_args.crossover_percent,
-                    mutation_percent: cli.basic_args.mutation_percent,
-                    n_generations,
-                    fitness_parameters: InteractiveLearningParameters::new(initial_states, input),
-                    program_parameters: QProgramGeneratorParameters::new(
-                        ProgramGeneratorParameters::new(
-                            cli.program_parameter.max_instructions,
-                            InstructionGeneratorParameters::from::<CartPoleInput>(
-                                cli.program_parameter
-                                    .instruction_generator_parameters
-                                    .n_extras,
-                                cli.program_parameter
-                                    .instruction_generator_parameters
-                                    .external_factor,
-                            ),
-                        ),
-                        consts,
+        let hyper_params = HyperParameters {
+            population_size: cli.basic_args.population_size,
+            gap: cli.basic_args.gap,
+            crossover_percent: cli.basic_args.crossover_percent,
+            mutation_percent: cli.basic_args.mutation_percent,
+            n_generations,
+            fitness_parameters: InteractiveLearningParameters::new(initial_states, input),
+            program_parameters: QProgramGeneratorParameters::new(
+                ProgramGeneratorParameters::new(
+                    cli.program_parameter.max_instructions,
+                    InstructionGeneratorParameters::from::<CartPoleInput>(
+                        cli.program_parameter
+                            .instruction_generator_parameters
+                            .n_extras,
+                        cli.program_parameter
+                            .instruction_generator_parameters
+                            .external_factor,
                     ),
-                };
+                ),
+                cli.basic_args.consts,
+            ),
+        };
 
-                let best_score = QLgp::build(hyper_params)
-                    .last()
-                    .as_ref()
-                    .and_then(|p| p.best())
-                    .map(|p| p.get_fitness().unwrap_or(f64::NAN))
-                    .unwrap_or(f64::NAN);
+        let best_score = QLgp::build(hyper_params)
+            .last()
+            .as_ref()
+            .and_then(|p| p.best())
+            .map(|p| p.get_fitness().unwrap_or(f64::NAN))
+            .unwrap_or(f64::NAN);
 
-                println!("{}", best_score)
-            }
-        }
+        println!("{}", best_score)
     }
 }
