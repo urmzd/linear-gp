1cc5890e51907ab58cade6f3113ad8a001862bd4
Urmzd Mukhammadnaim <urmzds@gmail.com>
2023-03-26 21:05:46 -0300

    fix: support loading configs

--- src/core/config.rs
+++ src/core/config.rs
@@ -1,24 +1,67 @@
-// pub fn load_config<T>(config_path: &str) -> HyperParameters<T>
-// where
-//     T: Fitness + Generate + Clone,
-// {
-//     let mut builder = Config::builder();
-//     builder = builder.set_default("population_size", 100).unwrap();
-//     builder = builder.set_default("gap", 0.5).unwrap();
-//     builder = builder.set_default("mutation_percent", 0.5).unwrap();
-//     builder = builder.set_default("crossover_percent", 0.5).unwrap();
-//     builder = builder.set_default("n_generations", 100).unwrap();
-//     builder = builder.add_source(File::new(config_path, FileFormat::Json));
-//     builder = builder.add_source(Environment::default());
+use crate::core::engines::status_engine::{Status, StatusEngine};
+use crate::{
+    core::engines::core_engine::HyperParameters,
+    problems::{
+        gym::{GymRsEngine, GymRsQEngine},
+        iris::IrisEngine,
+    },
+};
+use clap::Parser;
+use config::{Config, Environment, File, FileSourceFile};
+use gym_rs::envs::classical_control::{cartpole::CartPoleEnv, mountain_car::MountainCarEnv};
+use serde::{Deserialize, Serialize};
 
-//     // Add generic parameters and attempt to load into HyperParameters;
-//     todo!("hyper parameters")
-// }
+// Generate a macro which takes hyperparameters, builds the necessary engine and run its
+// outputting the best score for each generation
+macro_rules! run_accuator {
+    ($engine:ident, $hyperparameters:ident) => {
+        for population in $hyperparameters
+            .build_engine()
+            .take($hyperparameters.population_size)
+        {
+            println!("{}", StatusEngine::get_fitness(population.first().unwrap()));
+        }
+    };
+}
 
-// pub population_size: usize,
-// pub gap: f64,
-// pub mutation_percent: f64,
-// pub crossover_percent: f64,
-// pub n_generations: usize,
-// pub fitness_parameters: T::FitnessParameters,
-// pub program_parameters: T::GeneratorParameters,
+#[derive(Parser, Deserialize, Serialize)]
+pub enum Accuator {
+    MountainCarQ(HyperParameters<GymRsQEngine<MountainCarEnv, 3, 2>>),
+    MountainCarLGP(HyperParameters<GymRsEngine<MountainCarEnv, 3, 2>>),
+    Iris(HyperParameters<IrisEngine>),
+    CartPoleQ(HyperParameters<GymRsQEngine<CartPoleEnv, 4, 2>>),
+    CartPoleLGP(HyperParameters<GymRsEngine<CartPoleEnv, 4, 2>>),
+}
+
+impl Accuator {
+    pub fn run(&self) {
+        // Use the run engine macro for each branch of the enum
+        match self {
+            Accuator::MountainCarQ(hyperparameters) => {
+                run_accuator!(GymRsQEngine, hyperparameters)
+            }
+            Accuator::MountainCarLGP(hyperparameters) => {
+                run_accuator!(GymRsEngine, hyperparameters)
+            }
+            Accuator::Iris(hyperparameters) => {
+                run_accuator!(IrisEngine, hyperparameters)
+            }
+            Accuator::CartPoleQ(hyperparameters) => {
+                run_accuator!(GymRsQEngine, hyperparameters)
+            }
+            Accuator::CartPoleLGP(hyperparameters) => {
+                run_accuator!(GymRsEngine, hyperparameters)
+            }
+        }
+    }
+}
+
+pub fn load_configuration(filename: &str) -> Result<Accuator, Box<dyn std::error::Error>> {
+    let settings = Config::builder()
+        .add_source(File::with_name(filename))
+        .add_source(Environment::default())
+        .build()?;
+
+    let accuator: Accuator = settings.try_deserialize()?;
+    Ok(accuator)
+}
