2842fff74da70ae4430982f2d6974e59b5814e4b
Urmzd Mukhammadnaim <hello@urmzd.com>
2023-03-29 00:32:03 -0300

    fix: remove hyper parameter search to allow experimention

--- scripts/run_baseline.sh
+++ scripts/run_baseline.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+mkdir -p "assets/experiments/baseline/figures"
+
 # This script is used to run the baseline experiments for the paper.
 BENCHMARK_PREFIX="/tmp/iris" cargo nextest run iris --no-capture --release
 
 ./scripts/asset_generator.py --input "/tmp/iris" --output "/tmp/iris-tables" tables
-
-mkdir -p "assets/experiments/baseline/figures"
 
 for path in /tmp/iris-tables/*.csv; do
     echo "Processing $path"
