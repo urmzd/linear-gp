d5138acc980f1a2ee2b15ee5c10ec1f35ae213b9
Urmzd Mukhammadnaim <hello@urmzd.com>
2023-03-29 03:46:33 -0300

    fix: ensure baseline exists

--- scripts/run_baseline.sh
+++ scripts/run_baseline.sh
@@ -2,16 +2,9 @@
 
 mkdir -p "assets/experiments/baseline/figures"
 
-# This script is used to run the baseline experiments for the paper.
-BENCHMARK_PREFIX="/tmp/iris" cargo nextest run iris --no-capture --release
+BENCHMARK_PREFIX="assets/tmp" cargo nextest run iris --no-capture --release
 
-./scripts/asset_generator.py --input "/tmp/iris" --output "/tmp/iris-tables" tables
-
-for path in /tmp/iris-tables/*.csv; do
-    echo "Processing $path"
-    mv "$path" "assets/experiments/baseline/"
-done
-
+./scripts/asset_generator.py --input "assets/tmp" --output "assets/experiments/baseline" tables
 ./scripts/asset_generator.py --input "assets/experiments/baseline" --output "assets/experiments/baseline/figures" figures
 
-ls -l "assets/experiments/baseline"
+rm -rf "assets/tmp"
