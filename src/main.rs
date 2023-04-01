af5c4b357c9a0ca01fee703fc71df27f8d31f253
Urmzd Mukhammadnaim <hello@urmzd.com>
2023-04-01 20:40:40 -0300

    docs: hyperparameters section

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
-use lgp::core::config::Accuator;
+use lgp::core::config::Actuator;
 
 fn main() {
-    let mut cli = Accuator::parse();
+    let mut cli = Actuator::parse();
     cli.run();
 }
